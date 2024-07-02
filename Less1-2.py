66bcd7547767acf1e92926da0f238f4b624b2ef0
MstrDzen <MstrDzen@gmail.com>
2024-07-02 23:04:28 +0500

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ Less1-2.py
@@ -0,0 +1,4 @@
+homework = 12
+time_for_work = 1.5
+curse = 'Python'
+print('Курс: ', curse, ', всего задач: ', homework, ', затрачено часов: ', time_for_work, ', среднее время выполнения: ', time_for_work / homework, 'часа.')
